28714752c0b1573542f6b9c8082d84e132e6c4e5
Julie Kallini <jkallini@princeton.edu>
2020-02-29 23:40:08 -0500

    Add basic flask templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
