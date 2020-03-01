26ab36e999ae86de2489abcccde226ebea0958e6
Julie Kallini <jkallini@princeton.edu>
2020-03-01 14:15:33 -0500

    Add FSM editor

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/editor')
+def editor():
+    return render_template('editor.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
