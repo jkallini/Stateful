579f4c929d7b55c9bf9fbfe6985a2f02df6787dd
Julie Kallini <jkallini@princeton.edu>
2020-03-02 21:09:58 -0500

    Clean up code

---
+++ static/js/scale-canvas.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    //get canvas
+    let canv = document.getElementById('canvas');
+
+    const width = 800;
+    const height = 580;
+    const pixelRatio = window.devicePixelRatio || 1;
+
+    //get context
+    let ctx = canv.getContext('2d');
+
+    canv.width = width * pixelRatio;
+    canv.height = height * pixelRatio;
+
+    canv.style.width = `${width}px`;
+    canv.style.height = `${height}px`;
+
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+
+    ctx.scale(pixelRatio, pixelRatio);
+});
