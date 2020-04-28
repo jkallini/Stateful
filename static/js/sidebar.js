009e11be6021680efb0e04f02eb91a71b9819744
Julie Kallini <jkallini@princeton.edu>
2020-04-28 16:03:57 -0700

    Dismiss button on sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -4,25 +4,15 @@ $(document).ready(function() {
         theme: "minimal"
     });
 
-    $('#sidebarCollapse').on('click', function() {
-        $('#sidebar').toggleClass('active');
-    });
-
-});
-
-$(document).ready(function() {
-
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
-    $('#sidebarCollapse').on('click', function() {
-        // open or close navbar
-        $('#sidebar').toggleClass('active');
-        // close dropdowns
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
-        // and also adjust aria-expanded attributes we use for the open/closed arrows
-        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $('#dismiss').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
